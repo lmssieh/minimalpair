be3c670c25ec0420d83fac79a802a040613c0eb0
rust <53446488+lmssieh@users.noreply.github.com>
2022-09-15 16:41:12 +0100

    remove css from the script and import them using and url

--- script.user.js
+++ script.user.js
@@ -3,7 +3,9 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://forvo.com/*
 // @require     https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.iife.js
-// @grant       none
+// @resource   IMPORTED_CSS https://raw.githubusercontent.com/lmssieh/minimalpair/main/styles/styles.css
+// @grant      GM_getResourceText
+// @grant      GM_addStyle
 // @version     1.0
 // @author      -
 // @description minimalpair
@@ -12,131 +14,12 @@
 (function () {
 	if (window.self !== window.top) return;
 
-	const css = `
-	.top-banner {
-		background: black;
-		color: white;
-		text-align: center;
-		font-size: 120%;
-		padding: 0.5em 0;
-		cursor: pointer;
-		text-decoration : underline;
-		font-family: monospace ;
-	}
-	.minimalpair-iframe {
-		display: none;
-	}
-	#minimalpair {
-		font-family: monospace;
-		position: absolute;
-    background: #000;
-    z-index: 99999;
-    color: white;
-    width: 100%;
-    min-height: 100vh;
-		display: flex;
-		flex-direction: column;
-}
-
-#minimalpair button {
-	color: white;
-	border: solid 2px white;
-	background: none;
-	font-family: inherit;
-	padding: 0.5em 1em;
-	border-radius: 3px;
-	font-weight: bold;
-	line-height: 1em;
-
-}
-#minimalpair div {
-	max-width: 600px;
-	margin: 0 auto;
-}
-
-.minimalpair__topbar {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: .5em 0;
-}
-
-.minimalpair__topbar h1 {
-	font-size: 120%;
-	background: linear-gradient(to bottom right,#40e0d0,#f1be44,#ff0080);
-	padding: 0.5em 1em;
-	font-weight: bold;
-	color: black;
-}
-
-.minimalpair__topbar button {
-	padding: 0.2em .4em;
-
-}
-
-#closeMinimalPairApp {
-	margin-left: auto;
-}
-  .minimalpair__header {
-		padding: 2em 0;
-	}
-	.minimalpair__header div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-}
-
-.minimalpair__header h2 {
-	padding: 1em 0;
-}
-
-.minimalpair__words {
-	padding: 2em 0;
-}
-    .minimalpair__words ul {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-			gap: 1em;
-      padding: 0;
-      list-style: none;
-    }
-
-    .minimalpair__words ul li {
-      padding: 2rem;
-      border: 2px solid white;
-			background: white;
-			color: black;
-      cursor: pointer;
-      border-radius: 5px;
-      transition: all .3s ease;
-  }
-
-	.minimalpair__words ul li.active,
-  .minimalpair__words ul li:hover {
-    box-shadow: aliceblue 4px 4px;
-    background: black;
-    color: white;
-}
-
-#result {
-	padding: 1em 0;
-}
-
-.hidden {
-	display: none;
-}
-  `;
-	const style = document.createElement("style");
-	style.innerHTML = css;
-	document.head.append(style);
+	const css = GM_getResourceText("IMPORTED_CSS");
+	GM_addStyle(css);
 
 	window.PetiteVue = PetiteVue;
 
 	// utils
-
 	const { createApp } = PetiteVue;
 
 	const root = document.createElement("div");
@@ -177,8 +60,7 @@
 `;
 	document.querySelector("#wrap").insertAdjacentElement("afterBegin", root);
 
-	const words = 
-`stake stock stick stuck stack
+	const words = `stake stock stick stuck stack
 here hair hire
 sheet shit
 bad bed bud
@@ -227,7 +109,9 @@ skill skeel
 skim scheme
 sit seat
 scenic cynic
-bit beat`.split('\n').map(e=>e.split(' '));
+bit beat`
+		.split("\n")
+		.map((e) => e.split(" "));
 
 	function loadIframe(src) {
 		var iframe = document.createElement("iframe");
@@ -278,13 +162,19 @@ bit beat`.split('\n').map(e=>e.split(' '));
 				let sounds = a.contentDocument.querySelectorAll(
 					" article.pronunciations .play"
 				);
-				sounds = Array.from(sounds).filter(e=>e.parentNode.classList.contains('en_usa')||e.parentNode.classList.contains('en_uk'));
-				if(!sounds.length>0) { alert('whoops, found no sound for ' + this.randomWord); }
-				let that=this;
-				this.playWordSound = function(...args) {
-					if(!sounds.length) return;
-					let selected=that.getRandomItemFromArrray([...sounds]);
-					selected.onclick.apply(this,args);
+				sounds = Array.from(sounds).filter(
+					(e) =>
+						e.parentNode.classList.contains("en_usa") ||
+						e.parentNode.classList.contains("en_uk")
+				);
+				if (!sounds.length > 0) {
+					alert("whoops, found no sound for " + this.randomWord);
+				}
+				let that = this;
+				this.playWordSound = function (...args) {
+					if (!sounds.length) return;
+					let selected = that.getRandomItemFromArrray([...sounds]);
+					selected.onclick.apply(this, args);
 				};
 				console.log(this.playWordSound);
 				return;
